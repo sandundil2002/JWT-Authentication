caaa9d2e2fc305b0ff5084f27a22bf7241e04547
T.H. Sandun Dilshan <Sandundil2002@gmail.com>
2024-08-21 10:46:02 +0530

    user serviceIMPL implemented.

--- src/main/java/lk/ijse/jwtauthentication/dto/UserDTO.java
+++ src/main/java/lk/ijse/jwtauthentication/dto/UserDTO.java
@@ -3,10 +3,12 @@ package lk.ijse.jwtauthentication.dto;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.stereotype.Component;
 
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
+@Component
 public class UserDTO {
     private String email;
     private String password;
--- src/main/java/lk/ijse/jwtauthentication/service/impl/UserServiceIMPL.java
+++ src/main/java/lk/ijse/jwtauthentication/service/impl/UserServiceIMPL.java
@@ -1,16 +1,71 @@
 package lk.ijse.jwtauthentication.service.impl;
 
 import lk.ijse.jwtauthentication.dto.UserDTO;
+import lk.ijse.jwtauthentication.entity.User;
+import lk.ijse.jwtauthentication.repository.UserRepository;
 import lk.ijse.jwtauthentication.service.UserService;
+import lk.ijse.jwtauthentication.util.VarList;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-public class UserServiceIMPL implements UserService {
+import java.util.HashSet;
+import java.util.Set;
+
+@Service
+@Transactional
+public class UserServiceIMPL implements UserDetailsService, UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private ModelMapper modelMapper;
+
     @Override
     public int saveUser(UserDTO userDTO) {
-        return 0;
+        if (userRepository.existsByEmail(userDTO.getEmail())) {
+            return VarList.Not_Acceptable;
+        } else {
+            BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+            userDTO.setPassword(passwordEncoder.encode(userDTO.getPassword()));
+            UserDTO.setRole("DASH_ADMIN");
+            userRepository.save(modelMapper.map(userDTO, User.class));
+            return VarList.Created;
+        }
     }
 
     @Override
     public UserDTO searchUser(String username) {
-        return null;
+        if (userRepository.existsByEmail(username)) {
+            User user = userRepository.findByEmail(username);
+            return modelMapper.map(user, UserDTO.class);
+        } else {
+            return null;
+        }
     }
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(email);
+        return new org.springframework.security.core.userdetails.User(user.getEmail(), user.getPassword(), getAuthority(user));
+    }
+
+    public UserDTO loadUserDetailsByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(username);
+        return modelMapper.map(user, UserDTO.class);
+    }
+
+    private Set<SimpleGrantedAuthority> getAuthority(User user) {
+        Set<SimpleGrantedAuthority> authorities = new HashSet<>();
+        authorities.add(new SimpleGrantedAuthority(user.getRole()));
+        return authorities;
+    }
+
 }
