65a547f3213d366339e0dbebf427ffd57da086f6
T.H. Sandun Dilshan <Sandundil2002@gmail.com>
2024-08-21 12:45:36 +0530

    jwtFilter class implemented.

--- src/main/java/lk/ijse/jwtauthentication/config/JwtFilter.java
+++ src/main/java/lk/ijse/jwtauthentication/config/JwtFilter.java
@@ -1,4 +1,68 @@
 package lk.ijse.jwtauthentication.config;
 
-public class JwtFilter {
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lk.ijse.jwtauthentication.service.impl.UserServiceIMPL;
+import lk.ijse.jwtauthentication.util.JwtUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+public class JwtFilter extends OncePerRequestFilter {
+    @Autowired
+    private JwtUtil jwtUtil;
+
+    @Autowired
+    private UserServiceIMPL userService;
+
+    @Value("${jwt.header}")
+    private String secretKey;
+    @Autowired
+    private HttpServletRequest httpServletRequest;
+    @Autowired
+    private HttpServletResponse httpServletResponse;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        String authorization = httpServletRequest.getHeader("Authorization");
+        String token = null;
+        String email = null;
+
+        if (null != authorization && authorization.startsWith("Bearer ")) {
+            token = authorization.substring(7);
+            email = jwtUtil.getUserNameFromToken(token);
+            Claims claims = jwtUtil.getUserRoleCodeFromToken(token);
+            httpServletRequest.setAttribute("email", email);
+            httpServletRequest.setAttribute("role", claims.get("role"));
+        }
+
+        if (null != email && SecurityContextHolder.getContext().getAuthentication() == null) {
+            UserDetails userDetails = userService.loadUserByUsername(email);
+
+            if (jwtUtil.validateToken(token, userDetails)) {
+                UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+                usernamePasswordAuthenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(httpServletRequest));
+                SecurityContextHolder.getContext().setAuthentication(usernamePasswordAuthenticationToken);
+
+            }
+        }
+
+        filterChain.doFilter(httpServletRequest, httpServletResponse);
+    }
+
+    private Claims getClaimsFromJwtToken(String token) {
+        return Jwts.parser().setSigningKey(secretKey.getBytes()).parseClaimsJws(token).getBody();
+    }
 }
--- src/main/java/lk/ijse/jwtauthentication/util/JwtUtil.java
+++ src/main/java/lk/ijse/jwtauthentication/util/JwtUtil.java
@@ -4,7 +4,7 @@ import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import lk.ijse.jwtauthentication.dto.UserDTO;
-import lombok.Value;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
@@ -30,7 +30,8 @@ public class JwtUtil implements Serializable {
     }
 
     public Date getExpirationDateFromToken(String token) {
-        return getUserRoleCodeFromToken(token, Claims::getExpiration);
+//        return getUserRoleCodeFromToken(token, Claims::getExpiration);
+        return getUserRoleCodeFromToken(token).getExpiration();
     }
 
     public <T> T getClaimFromToken(String token, Function<Claims, T> claimsResolver) {
@@ -53,15 +54,18 @@ public class JwtUtil implements Serializable {
         return doGenerateToken(claims, userDTO.getEmail());
     }
 
-    public String doGenerateToken(Map<String, Object> claims , String subject){ {
+    public String doGenerateToken(Map<String, Object> claims , String subject) {
         return Jwts.builder().setClaims(claims).setSubject(subject).setIssuedAt(new Date(System.currentTimeMillis()))
                 .setExpiration(new Date(System.currentTimeMillis() + JWT_TOKEN_VALIDITY * 1000))
-                .signWith(SignatureAlgorithm.HS512,secretKey).compact();
+                .signWith(SignatureAlgorithm.HS512, secretKey).compact();
     }
 
     public Boolean validateToken(String token, UserDetails userDetails) {
-        final String username = getUsernameFromToken(token);
-        return (username.equals(userDetails.getUsername() && !isTokenExpired(token));
+        final String username = getUserNameFromToken(token);
+        return (username.equals(userDetails.getUsername()) && !isTokenExpired(token));
     }
 
+    public String getUserNameFromToken(String token) {
+        return getClaimFromToken(token, Claims::getSubject);
+    }
 }
