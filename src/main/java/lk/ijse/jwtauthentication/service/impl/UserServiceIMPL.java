4aa90a89b51c2be1a0f0377eb3d220492a983afd
T.H. Sandun Dilshan <Sandundil2002@gmail.com>
2024-08-21 10:14:31 +0530

    user service created.

---
+++ src/main/java/lk/ijse/jwtauthentication/service/impl/UserServiceIMPL.java
@@ -0,0 +1,16 @@
+package lk.ijse.jwtauthentication.service.impl;
+
+import lk.ijse.jwtauthentication.dto.UserDTO;
+import lk.ijse.jwtauthentication.service.UserService;
+
+public class UserServiceIMPL implements UserService {
+    @Override
+    public int saveUser(UserDTO userDTO) {
+        return 0;
+    }
+
+    @Override
+    public UserDTO searchUser(String username) {
+        return null;
+    }
+}
