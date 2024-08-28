dc96fabf4b935cc8b515dc29c7369690f6838090
T.H. Sandun Dilshan <Sandundil2002@gmail.com>
2024-08-28 12:24:09 +0530

    token through get function implement.

--- Backend/src/main/java/lk/ijse/jwtauthentication/controller/BlogController.java
+++ Backend/src/main/java/lk/ijse/jwtauthentication/controller/BlogController.java
@@ -1,11 +1,13 @@
 package lk.ijse.jwtauthentication.controller;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("api/v1/blog")
+@CrossOrigin
 public class BlogController {
     @GetMapping("/newMethod")
     public String newMethod(){
--- Frontend/js/dashboard.js
+++ Frontend/js/dashboard.js
@@ -1,11 +1,29 @@
 $(document).ready(function() {
     let token = localStorage.getItem('token');
-    if (token == null) {
-        window.location.href = 'login.html';
+    if (token) {
+        $.ajax({
+            url: 'http://localhost:8080/api/v1/blog/newMethod',
+            method: 'GET',
+            contentType: 'application/json',
+            headers: {
+                'Authorization': 'Bearer ' + token
+            },
+
+            success: function (response) {
+                $('#hello').append(response);
+                console.log(response);
+            },
+
+            error: function (error) {
+                console.log(error);
+            }
+        });
+    } else {
+        window.location.href = 'signin.html';
     }
 })
 
 function userLogout() {
     localStorage.removeItem('token');
-    window.location.href = 'login.html';
+    window.location.href = 'signin.html';
 }
