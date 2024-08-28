7522e2b9c18afae1d4b3996f050222dd79cfc85f
T.H. Sandun Dilshan <Sandundil2002@gmail.com>
2024-08-28 09:12:55 +0530

    frontend module created.

--- src/main/java/lk/ijse/jwtauthentication/service/UserService.java
+++
@@ -1,8 +0,0 @@
-package lk.ijse.jwtauthentication.service;
-
-import lk.ijse.jwtauthentication.dto.UserDTO;
-
-public interface UserService {
-    int saveUser(UserDTO userDTO);
-    UserDTO searchUser(String username);
-}
